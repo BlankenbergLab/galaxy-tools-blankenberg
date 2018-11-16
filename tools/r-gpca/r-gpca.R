7802eaaf31c7f77a1c5eb523947e3803d347acdc
Daniel Blankenberg <dan.blankenberg@gmail.com>
2018-11-16 14:21:10 -0500

    Update gPCA tool. Need to shrink input test data and rebuild test outputs.

--- tools/r-gpca/r-gpca.R
+++ tools/r-gpca/r-gpca.R
@@ -5,6 +5,12 @@ library(gPCA)
 
 # parse options
 option_list = list(
+   make_option(
+    c("--version"),
+    action = "store_true",
+    default = FALSE,
+    help = "Print version and exit"
+   ),
   make_option(
     c("-i", "--batch"),
     action = "store",
@@ -28,25 +34,30 @@ option_list = list(
    ),
    make_option(
     c("-c", "--center"),
-    action = "store",
-    default = 'FALSE',
-    type = 'character',
-    help = "Is the input batch and data centered"
+    action = "store_true",
+    default = FALSE,
+    help = "Is the input batch and data centered [default %default]"
 	 ),
    make_option(
     c("-y", "--scaleY"),
-    action = "store",
-    default = 'FALSE',
-    type = 'character',
+    action = "store_true",
+    default = FALSE,
     help = "Scale to Y axis"
    ),
    make_option(
     c("-f", "--filt"),
     action = "store",
-    default = '',
-    type = 'character',
+    default = NULL,
+    type = 'numeric',
     help = "Retain features"
 	 ),
+   make_option(
+    c( "--npcs"),
+    action = "store",
+    default = 3,
+    type = 'numeric',
+    help = "Number of principal compoents to plot"
+   ),
    make_option(
     c("-p", "--path"),
     action = "store",
@@ -57,8 +68,8 @@ option_list = list(
    make_option(
     c("-s", "--seed"),
     action = "store",
-    default = '',
-    type = 'character',
+    default = NULL,
+    type = 'numeric',
     help = "Set a seed number"
    ),
    make_option(
@@ -79,6 +90,12 @@ option_list = list(
 
 opt <-parse_args(OptionParser(option_list = option_list))
 
+if (opt$version){
+  # print version and exit
+  cat(paste("gPCA version", toString(packageVersion("gPCA"))), "\n")
+  quit()
+}
+
 # Check parameter values
 
 if ( ! file.exists(opt$batch)){
@@ -92,15 +109,7 @@ batch<-as.numeric(factor(as.matrix(read.delim(opt$batch, header=TRUE, row.names=
 
 data<-t(as.matrix(read.delim(opt$data, row.names=1, header=TRUE, sep="\t")))
 
-nperm<-opt$nperm
-center<-opt$center
-scaleY<-opt$scaleY
-
-if (opt$filt == "") {filt<-NULL} else {filt<-opt$filt};
-
-if (opt$seed == "") {seed<-NULL} else {seed<-opt$seed};
-
-out<-gPCA.batchdetect(x=data, batch=batch, center=center, scaleY=scaleY, nperm=nperm, filt=filt, seed=seed)
+out<-gPCA.batchdetect(x=data, batch=batch, center=opt$center, scaleY=opt$scaleY, nperm=opt$nperm, filt=opt$filt, seed=opt$seed)
 
 out$varPCg1<-((out$varPCg1-out$varPCu1)/out$varPCg1)*100
 
@@ -129,13 +138,13 @@ invisible(dev.off())
 
 # Guided/Unguided comp,3
 par(mai=c(0.65,0.65,0.1,0.1),cex=0.8)
-png(paste(opt$path,'/guided_comp_3.png', sep=""), width=1020, height=800, units='px')
-PCplot(out,ug="guided",type="comp",npcs=3)
+png(paste(opt$path,'/guided_npcs.png', sep=""), width=1020, height=800, units='px')
+PCplot(out,ug="guided",type="comp",npcs=opt$npcs)
 invisible(dev.off())
 
 par(mai=c(0.65,0.65,0.1,0.1),cex=0.8)
-png(paste(opt$path,'/unguided_comp_3.png', sep=""), width=1020, height=800, units='px')
-PCplot(out,ug="unguided",type="comp",npcs=3)
+png(paste(opt$path,'/unguided_npcs.png', sep=""), width=1020, height=800, units='px')
+PCplot(out,ug="unguided",type="comp",npcs=opt$npcs)
 invisible(dev.off())
 
 
@@ -151,27 +160,27 @@ CumulativeVarPlot(out,ug="unguided",col="blue")
 invisible(dev.off())
 
 
-     write(paste('<html>
-      <table id="r-gpca-wrap" align="center" border="1">
-       <tr>
-        <th>Delta</th><th>P-value</th><th>varPCg1</th>
-       </tr>
-       <tr>
-        <td id=delta>',out$delta,'</td><td id=p.val>',out$p.val,'</td><td id=varPCg1>',out$varPCg1,'</td>
-       </tr>
-      </table>
+write(paste('<html>
+<table id="r-gpca-wrap" align="center" border="1">
+ <tr>
+  <th>Delta</th><th>P-value</th><th>varPCg1</th>
+ </tr>
+ <tr>
+  <td id=delta>',out$delta,'</td><td id=p.val>',out$p.val,'</td><td id=varPCg1>',out$varPCg1,'</td>
+ </tr>
+</table>
 
-      <center><img src="gDist.png"/></center><br>
-      <center><title>Guided 1v2</title><br><img src="guided_1v2.png"/></center><br>
-      <center><title>Unguided 1v2</title><br><img src="unguided_1v2.png"/></center><br>
+<center><img src="gDist.png"/></center><br>
+<center><title>Guided 1v2</title><br><img src="guided_1v2.png"/></center><br>
+<center><title>Unguided 1v2</title><br><img src="unguided_1v2.png"/></center><br>
 
-      <center><title>Guided Compare to 3</title><br><img src="guided_comp_3.png"/></center><br>
-      <center><title>Unguided Compare to 3</title><br><img src="unguided_comp_3.png"/></center><br>
+<center><title>Guided Compare to ',opt$npcs,'</title><br><img src="guided_npcs.png"/></center><br>
+<center><title>Unguided Compare to ',opt$npcs,'</title><br><img src="unguided_npcs.png"/></center><br>
 
-      <center><title>Guided Cumulative Variance</title><br><img src="guided_var.png"/></center><br>
-      <center><title>Unguided Cumulative Variance</title><br><img src="unguided_var.png"/></center><br>
- 
-      </html>'
-      ),file = opt$html_outfile)
+<center><title>Guided Cumulative Variance</title><br><img src="guided_var.png"/></center><br>
+<center><title>Unguided Cumulative Variance</title><br><img src="unguided_var.png"/></center><br>
+
+</html>'
+),file = opt$html_outfile)
 
 sessionInfo()
